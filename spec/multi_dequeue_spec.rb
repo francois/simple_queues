4c09ca42d0cb5cf95821d076591e9486f11f50e2
François Beausoleil <francois@teksol.info>
2011-03-26 22:30:02 -0400

    Minor cleanups

--- spec/multi_dequeue_spec.rb
+++ spec/multi_dequeue_spec.rb
@@ -27,14 +27,14 @@ describe SimpleQueues::Redis, "multiple dequeue" do
   end
 
   context "#dequeue_with_timeout" do
-    it "should return the queue name when a message was dequeued" do
+    it "should return the queue name where the message was dequeued" do
       queue.on_dequeue(:a) {|message| message}
       queue.on_dequeue(:b) {|message| raise "not here"}
       queue.enqueue(:a, :sent_to => "a")
       queue.dequeue_with_timeout(1).should == "a"
     end
 
-    it "should return nil when no queues returned anything" do
+    it "should return nil when no messages were pending" do
       queue.on_dequeue(:a) {|message| raise "not here"}
       queue.dequeue_with_timeout(1).should be_nil
     end
@@ -47,9 +47,8 @@ describe SimpleQueues::Redis, "multiple dequeue" do
       queue.enqueue(:a, "sent_to" => "a", "serial" => 1)
       queue.enqueue(:b, "sent_to" => "b", "serial" => 1)
       queue.enqueue(:a, "sent_to" => "a", "serial" => 2)
-      while queue.dequeue_with_timeout(1)
-        # NOP
-      end
+
+      3.times { queue.dequeue_with_timeout(1) }
 
       a.should == [{"sent_to" => "a", "serial" => 1}, {"sent_to" => "a", "serial" => 2}]
       b.should == [{"sent_to" => "b", "serial" => 1}]
